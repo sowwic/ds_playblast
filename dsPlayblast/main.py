6ca6eb16e666e34b0c613f6cec9f2b2d063a126b
S0nic014 <loki.di90@gmail.com>
2020-07-25 11:28:29 +0100

    Added qdarkstyle package

--- dsPlayblast/main.py
+++ dsPlayblast/main.py
@@ -1,26 +1,32 @@
 import os
 import logging
 from PySide2 import QtWidgets
+import qdarkstyle
 from dsPlayblast import clientFn
 from dsPlayblast import playblastFn
 from dsPlayblast import playblastUI
 from dsPlayblast import loggingFn
 
-# Start application
-app = QtWidgets.QApplication(os.sys.argv)
 
-# Logging
-loggingFn.setup_logging()
-LOGGER = logging.getLogger(__name__)
+def main():
+    # Init application
+    app = QtWidgets.QApplication(os.sys.argv)
+    # app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api="pyside2"))
 
-# Init interface
-mainWindow = playblastUI.PlayblastWindow()
-mainWindow.always_on_top = True
-mainWindow.show()
+    # Logging
+    loggingFn.setup_logging()
+    LOGGER = logging.getLogger(__name__)
+
+    # Init interface
+    mainWindow = playblastUI.PlayblastWindow()
+    mainWindow.setPalette(styles.darkPallete())
+    mainWindow.show()
+
+    # Debug
+    LOGGER.info(f"Settings: {mainWindow.settings.fileName()}")
+    LOGGER.info(f"Always on top: {mainWindow.always_on_top}")
+
+    os.sys.exit(app.exec_())
 
 
-# Debug
-LOGGER.info(f"Settings: {mainWindow.settings.fileName()}")
-LOGGER.info(f"Always on top: {mainWindow.always_on_top}")
-
-os.sys.exit(app.exec_())
+main()
